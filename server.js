fb9836742ee21fc7e88e6c7bc4c1953597ffd8f6
Hem Shrestha <hereshem@gmail.com>
2017-12-09 12:43:28 +0545

    server added

---
+++ server.js
@@ -0,0 +1,54 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
+
+var messages = [];
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/jquery-1.11.1.js', function(req, res){
+  res.sendFile(__dirname + '/jquery-1.11.1.js');
+});
+
+app.get('/socket.io-1.2.0.js', function(req, res){
+  res.sendFile(__dirname + '/socket.io-1.2.0.js');
+});
+
+io.on('connection', function(socket){
+	var id = socket.id;
+	socket.on('join', function(name){
+    console.log(name + " joined");
+		socket.name = name;
+		socket.room = "myroom";
+		socket.join(socket.room);
+    io.in(socket.room).emit('join', name);
+    for (var i = 0; i < messages.length; i++) {
+      socket.emit('message', messages[i]);
+    }
+	});
+	
+  socket.on('message', function(msg){
+  	if(msg){
+      var message = {name:socket.name,message:msg,timestamp:new Date().getTime()};
+  		console.log("<" + message.name + "> " + msg);
+      messages.push(message);
+  		if(messages.length > 100){
+  			messages.shift();
+  		}
+  		io.in(socket.room).emit('message', message);
+	    // io.socket(id).emit('message', message);
+		  // io.emit('message', message);
+	}
+  });
+  socket.on('typing', function(msg){
+    var message = {name:socket.name,message:msg,timestamp:new Date().getTime()};
+    io.in(socket.room).emit('typing', message);
+  });
+});
+
+http.listen(port, function(){
+  console.log('Listening on port ' + port);
+});
